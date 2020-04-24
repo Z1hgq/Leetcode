84e3d4510fc5be6403972e64be995c32b7c7447f
gqzhang <z11hgq@gmail.com>
2020-04-24 15:03:33 +0800

    dp: maxSubArray

---
+++ dp/maxSubArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function (nums) {
+  let max = nums[0];
+  const dp = new Array(nums.length);
+  dp[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (dp[i - 1] > 0) dp[i] = dp[i - 1] + nums[i];
+    else dp[i] = nums[i];
+    max = Math.max(dp[i], max);
+  }
+  return max;
+};
+
+const arr = [-2, 1, 3, 4, -1, 2, 1, -5, 4];
+
+console.log(maxSubArray(arr));
