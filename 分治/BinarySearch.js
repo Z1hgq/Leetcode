b2ce2e4251fc7fc07551e79c43ce73f7b4d82aa2
gqzhang <z11hgq@gmail.com>
2020-04-30 11:36:19 +0800

    RecursiveBinarySearch

--- 分治/BinarySearch.js
+++ 分治/BinarySearch.js
@@ -13,3 +13,15 @@ function BinarySearch(arr, key) {
   }
   return -1;
 }
+function RecursiveBinarySearch(arr, low, high, key) {
+  const mid = Math.floor((low + high) / 2) + low;
+  if (arr[mid] == key) return mid;
+  if (low >= high) {
+    return -1;
+  } else if (key > arr[mid]) {
+    return RecursiveBinarySearch(arr, mid + 1, high, key);
+  } else if (key < arr[mid]) {
+    return RecursiveBinarySearch(arr, low, mid - 1, key);
+  }
+  return -1;
+}
