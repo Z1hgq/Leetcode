c8c18b9307db9843d26a96561aedc4d43fecba5b
gqzhang <z11hgq@gmail.com>
2020-07-17 16:37:00 +0800

    排序算法：冒泡、快排

---
+++ front-end-interview/sort.js
@@ -0,0 +1,75 @@
+/********************
+ * 常用排序算法实现
+ * 参考链接：https://www.cnblogs.com/onepixel/articles/7674659.html
+ ********************/
+/**
+ * 交换两个数在数组中的位置
+ * @param {array} arr 
+ * @param {number} i 
+ * @param {number} j 
+ */
+function swap (arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+/**
+ * 交换类：冒泡排序
+ * @param {array} arr 
+ * @returns {array}
+ */
+function bubbleSort (arr = []) {
+    const len = arr.length;
+    if (len < 2) {
+        return arr;
+    }
+    for (let i = 0; i < len; i++) {
+        for (j = 0; j < len - 1 - i; j++) {
+            if (arr[i] >= arr[j+1]) {
+                swap(arr, i, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+/**
+ * 交换类：快速排序
+ * @param {array} arr
+ * @param {number} left
+ * @param {number} right
+ * @returns {array}
+ */
+function quickSort(arr, left, right) {
+    const len = arr.length;
+    let partitionIndex;
+    const checkedLeft = typeof left === 'number' ? left : 0;
+    const checkedRight = typeof right === 'number' ? right : len - 1;
+    if (checkedLeft < checkedRight) {
+        partitionIndex = partition(arr, checkedLeft, checkedRight);
+        quickSort(arr, checkedLeft, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, checkedRight);
+    }
+    return arr;
+}
+/**
+ * 分区
+ * @param {array}} arr 
+ * @param {number} left 
+ * @param {number} right 
+ */
+function partition(arr, left, right) {
+    let pivot = left, index = pivot + 1;
+    // 以left下标的数为基数，往后遍历到right，中间若遇到比基数小的数那么从left+1的位置开始依次+1往后交换
+    for (let i = index; i <= right; i++) {
+        if (arr[i] < arr[pivot]) {
+            swap(arr, i, index);
+            index++;
+        }
+    }
+    // 最后将基数与比他小的最后一个数交换位置，那么最后的数组比基数小的都在它左边，比基数大的都在它右边
+    swap(arr, pivot, index - 1);
+    return index - 1;
+}
+
+ 
