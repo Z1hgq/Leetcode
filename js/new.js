b3e73615dd510b14eefdeec4921ccca717f965e4
Z1hgq <z11hgq@gmail.com>
2022-11-05 14:45:44 +0800

    feat: update CustomNew

--- js/new.js
+++ js/new.js
@@ -5,15 +5,37 @@
  * @LastEditTime: 2021-03-15 10:29:53
  */
 
-const objFactory = () => {
-    // 取出构造函数
-    const Constructor = [].shift.call(arguments);
-    // 创建一个新的空对象
-    const obj = Object.create(null);
-    // 将空对象的原型对象指向构造函数的原型
-    obj.__proto__ = Constructor.prototype;
-    // 绑定this到obj
-    const result = Constructor.call(obj, ...arguments);
-    // 返回结果必须是对象
-    return typeof result === 'object' ? result : obj;
+function CustomNew() {
+    // 参数的第一个是构造函数
+    const Constuctor = Array.prototype.shift.call(arguments) // 注意function函数才有arguments 属性
+
+    const obj = Object.create(null)
+
+    // 连接原型链
+    obj.__proto__ = Constuctor.prototype
+
+    const ret = Constuctor.apply(obj, [...arguments])
+
+    // 如果构造器有返回值，直接返回，否则返回创建的对象
+
+    return typeof ret === 'object' ? ret : obj
+
 };
+
+function Person(args) {
+    const { name, age } = args
+    this.name = name
+    this.age = age
+    this.getName = () => {
+        return this.name
+    }
+    this.getAge = () => {
+        return this.age
+    }
+}
+
+// const p = new Person({ name: 'z1hgq', age: 23 })
+const p = CustomNew(Person, { name: 'z1hgq', age: 23 })
+
+console.log(p.getName())
+console.log(p.getAge())
