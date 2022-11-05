19a40a5721f8091b09cbc172bcdb770db0ede980
Z1hgq <z11hgq@gmail.com>
2022-11-05 15:00:52 +0800

    feat: update CustomCall

--- js/call.js
+++ js/call.js
@@ -7,22 +7,25 @@
 
 // fn.call(obj, ...arguments)
 
-Function.prototype.myCall = function (context) {
-    context = context ? Object(context) : window;
-    context.fn = this; // 重置上下文, 这里的this就是调用myCall的函数
-    console.log(this);
-    const args = [...arguments].slice(1); // 获取参数
-    const result = context.fn(...args); // 执行函数
-    delete context.fn; // 删除添加的过渡性函数，避免对象属性污染
-    return result; // 返回结果
+Function.prototype.CustomCall = function (context, ...args) {
+    context = context || window
+    context.fn = this
+    const ret = context.fn(...args) // fn执行时 内部的this指向context
+    delete context.fn
+    return ret
 }
 
 // 浏览器环境下
-var a = 1, b = 2;
-var obj = { a: 10, b: 20 }
+const obj = { a: 10, b: 20 }
+
+globalThis.a = 1
+globalThis.b = 2
+
 function test(key1, key2) {
+    // this 默认指向globalThis
+    console.log(this)
     console.log(this[key1] + this[key2])
 }
 test('a', 'b') // 3
-test.myCall(obj, 'a', 'b') // 30
+test.CustomCall(obj, 'a', 'b') // 30
 
